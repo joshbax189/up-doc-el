3fc510f02ec7e94e3a035e4a4ab05ea66578d2a2
Josh Bax <joshbax189@gmail.com>
2026-09-13 21:22:07 -0700

    Bump versions and changelog

--- up-doc.el
+++ up-doc.el
@@ -2,7 +2,7 @@
 
 ;; Author: Josh Bax
 ;; Maintainer: Josh Bax
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "29.1") (dash "2.20.0"))
 ;; Homepage: https://github.com/joshbax189/up-doc-el
 ;; Keywords: lisp
