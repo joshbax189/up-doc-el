3f65f44a1d04a9f6538cb66345fb53f4267cf3ab
Josh Bax <joshbax189@gmail.com>
2026-09-18 09:44:17 -0700

    Add test for up-doc--delete-keyword

--- test/up-doc.test.el
+++ test/up-doc.test.el
@@ -605,4 +605,17 @@ _C-n_ext line  _a_ll              _R_efine               _C-z_: undo
     (should (equal (sexp-at-point)
                    '(my . #'foo)))))
 
+(ert-deftest up-doc--delete-keyword/test ()
+  "Tests basic behavior."
+  (should (equal (up-doc--delete-keyword '(use-package blah :bah a) :foo)
+                 '(use-package blah :bah a)))
+  (should (equal (up-doc--delete-keyword '(use-package blah :bah a) :bah)
+                 '(use-package blah)))
+  (should (equal (up-doc--delete-keyword '(use-package blah :bah ((x) (y) (z))) :bah)
+                 '(use-package blah)))
+  (should (equal (up-doc--delete-keyword '(use-package blah :bah ((x) (y) (z)) :foo z) :bah)
+                 '(use-package blah :foo z)))
+  (should (equal (up-doc--delete-keyword '(use-package blah :bah (x) (y) (z) :foo z) :bah)
+                 '(use-package blah :foo z))))
+
 ;;; up-doc.test.el ends here
